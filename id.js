fb08513ee8993a5a9dfb850ae61acf3806c8c3f8
Mr Mtaju <164275612+Lazack28@users.noreply.github.com>
2024-04-26 09:24:34 +0300

    Update id.js

--- id.js
+++ id.js
@@ -1,6 +1,6 @@
 function makeid(num = 4) {
   let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let characters = "Lazack-wer123ghbkjnhbf";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
